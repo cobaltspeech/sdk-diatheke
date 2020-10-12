edf180fd3bce1145fc7d2990dd35eab644524a27
Michael Plooster <mplooster@cobaltspeech.com>
2020-10-12 16:10:31 -0600

    Add the new APIv2 protobuf file and golang code.
    Includes the auto-generated go code (diathekepb) and updated SDK
    code around the grpc code.

--- grpc/go-diatheke/streams.go
+++ grpc/go-diatheke/streams.go
@@ -1,4 +1,4 @@
-// Copyright (2019) Cobalt Speech and Language Inc.
+// Copyright (2020) Cobalt Speech and Language Inc.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,76 +15,120 @@
 package diatheke
 
 import (
+	"io"
+
 	"github.com/cobaltspeech/sdk-diatheke/grpc/go-diatheke/diathekepb"
 )
 
-// AudioInputStream allows audio to be pushed to the server for a session.
-// The audio pushed to this stream will contribute to the session's
-// current dialog. Transcriptions are returned as Recognize events on
-// the session's event stream.
-type AudioInputStream struct {
-	PBStream diathekepb.Diatheke_StreamAudioInputClient
-}
-
-// Write the given audio data to the stream.
-func (ais *AudioInputStream) Write(p []byte) (n int, err error) {
-
-	// Setup the request
-	req := diathekepb.AudioInput{
-		Request: &diathekepb.AudioInput_Data{
-			Data: p,
-		},
-	}
-
-	// Send it on the stream.
-	err = ais.PBStream.Send(&req)
-	if err != nil {
-		return 0, err
-	}
-
-	return len(p), nil
-}
-
-// Finish notifies Diatheke that no more audio is coming from the client.
-func (ais *AudioInputStream) Finish() error {
-	_, err := ais.PBStream.CloseAndRecv()
-	return err
-}
-
-// ASRStream is a bi-directional stream that allows audio data to be pushed
-// to the server, while transcription results are streamed back to the client.
-// Audio pushed to an ASRStream does not contribute to any currently
-// running sessions and their dialogs. It is mainly useful for cases where
-// the client only wants a transcription without any other interactions with
-// Diatheke.
+// ASRStream represents a stream of data sent from the client to
+// Diatheke for the purpose of speech recognition.
 type ASRStream struct {
 	PBStream diathekepb.Diatheke_StreamASRClient
 }
 
-// Write the given audio data to the stream.
-func (asr *ASRStream) Write(p []byte) (n int, err error) {
-	// Setup the request
-	req := diathekepb.ASRRequest{
-		AsrData: &diathekepb.ASRRequest_Audio{
-			Audio: p,
+// SendAudio to Diatheke for transcription. If the returned error
+// is io.EOF, the stream has already been closed by the server and
+// Result() should be called to get the final result.
+func (asr *ASRStream) SendAudio(audio []byte) error {
+	return asr.PBStream.Send(&diathekepb.ASRInput{
+		Data: &diathekepb.ASRInput_Audio{
+			Audio: audio,
 		},
+	})
+}
+
+// SendToken passes the given session token to Diatheke to update
+// the speech recognition context. The session token must first be
+// sent on the ASR stream before any audio will be recognized. If
+// the stream  was created using client.NewSessionASRStream(), the
+//first token was already sent.
+//
+// If the returned error is io.EOF, the Result() function should be
+// called to get the final ASR result.
+func (asr *ASRStream) SendToken(token *diathekepb.TokenData) error {
+	return asr.PBStream.Send(&diathekepb.ASRInput{
+		Data: &diathekepb.ASRInput_Token{
+			Token: token,
+		},
+	})
+}
+
+// Result returns the result of speech recognition. This function
+// may be called to end the audio stream early, which will force
+// a transcription based on the audio received until this point,
+// or in response to receiving an io.EOF error from PushAudio.
+func (asr *ASRStream) Result() (*diathekepb.ASRResult, error) {
+	return asr.PBStream.CloseAndRecv()
+}
+
+// ReadASRAudio is a convenience function to send audio from the
+// given reader to the stream until a result is returned. If an
+// error is encountered no result will be returned.
+func ReadASRAudio(
+	stream *ASRStream, reader io.Reader, buffSize int,
+) (*diathekepb.ASRResult, error) {
+	buffer := make([]byte, buffSize)
+	for {
+		// Pull audio data from the reader
+		bytesRead, err := reader.Read(buffer)
+		if err == io.EOF {
+			// This indicates the reader has reached its end, in
+			// which case we want to end the stream and get a result.
+			break
+		} else if err != nil {
+			return nil, err
+		}
+
+		// Send the audio to the stream.
+		err = stream.SendAudio(buffer[0:bytesRead])
+		if err == io.EOF {
+			// The server closed the stream, which means we
+			// are ready to get a result.
+			break
+		} else if err != nil {
+			return nil, err
+		}
 	}
 
-	// Send it on the stream
-	err = asr.PBStream.Send(&req)
+	// Return the result
+	return stream.Result()
+}
+
+// TTSStream represents a stream of audio data from Diatheke to the
+// client containing synthesized speech.
+type TTSStream struct {
+	PBStream diathekepb.Diatheke_StreamTTSClient
+}
+
+// ReceiveAudio waits for the next chunk of audio data from the server.
+// When the returned error is io.EOF, synthesis is complete and no more
+// audio will be sent from Diatheke.
+func (tts *TTSStream) ReceiveAudio() ([]byte, error) {
+	reply, err := tts.PBStream.Recv()
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 
-	return len(p), nil
+	return reply.Audio, nil
 }
 
-// Receive waits for a transcript from the server.
-func (asr *ASRStream) Receive() (*diathekepb.ASRResponse, error) {
-	return asr.PBStream.Recv()
-}
+// WriteTTSAudio is a convenience function to receive audio from the
+// given TTSStream and send it to the writer until there is no more
+// audio to receive.
+func WriteTTSAudio(stream *TTSStream, writer io.Writer) error {
+	for {
+		// Wait for the next audio chunk
+		audio, err := stream.ReceiveAudio()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
 
-// AudioFinished notifies Diatheke that no more audio is coming from the client.
-func (asr *ASRStream) AudioFinished() error {
-	return asr.PBStream.CloseSend()
+		// Write the audio chunk
+		if _, err = writer.Write(audio); err != nil {
+			return err
+		}
+	}
 }
