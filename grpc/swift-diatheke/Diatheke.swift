d9c257dbe2817b4d43d1c610a914cf769d45180e
Eduard Miniakhmetov <eminiakhmetov@tatinfotech.com>
2020-10-20 23:00:03 +0300

    Swift SDK v2

--- grpc/swift-diatheke/Diatheke.swift
+++ grpc/swift-diatheke/Diatheke.swift
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-public let DiathekeVersion = "1.0"
+public let DiathekeSDKVersion = "2.0"
