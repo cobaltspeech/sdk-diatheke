8ea6bf744e882ec37d0afbcc39d714fa6aa29054
Michael Plooster <mplooster@cobaltspeech.com>
2021-08-12 14:54:27 -0600

    feat: Update go SDK with changes to gRPC API.
    Also made a couple of small modifications to the API itself that
    fix a couple of issues revealed during implementation.

--- grpc/go-diatheke/v2/streams.go
+++ grpc/go-diatheke/v2/streams.go
@@ -1,4 +1,4 @@
-// Copyright (2020) Cobalt Speech and Language Inc.
+// Copyright (2021) Cobalt Speech and Language Inc.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 package diatheke
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/cobaltspeech/sdk-diatheke/grpc/go-diatheke/v2/diathekepb"
@@ -131,4 +132,135 @@ func WriteTTSAudio(stream *TTSStream, writer io.Writer) error {
 			return err
 		}
 	}
+}
+
+// TranscribeStream represents a stream of data sent from the client
+// to Diatheke for the purpose of transcribing speech.
+type TranscribeStream struct {
+	PBStream diathekepb.Diatheke_TranscribeClient
+}
+
+// SendAudio to Diatheke for transcription. If the returned error
+// is io.EOF, the stream has already been closed by the server.
+func (ts *TranscribeStream) SendAudio(audio []byte) error {
+	return ts.PBStream.Send(&diathekepb.TranscribeInput{
+		Data: &diathekepb.TranscribeInput_Audio{
+			Audio: audio,
+		},
+	})
+}
+
+// SendAction passes the given transcribe action to Diatheke to use
+// for speech recognition. The action must first be sent before
+// any audio will be transcribed. If the stream was created using
+// client.NewTranscribeStream(), the action was already sent.
+func (ts *TranscribeStream) SendAction(action *diathekepb.TranscribeAction) error {
+	return ts.PBStream.Send(&diathekepb.TranscribeInput{
+		Data: &diathekepb.TranscribeInput_Action{
+			Action: action,
+		},
+	})
+}
+
+// ReceiveResult waits for the next transcribe result from
+// Diatheke. When the returned error is io.EOF, the stream has
+// been closed and no more results will be sent from Diatheke.
+func (ts *TranscribeStream) ReceiveResult() (*diathekepb.TranscribeResult, error) {
+	return ts.PBStream.Recv()
+}
+
+type TranscribeResultHandler func(*diathekepb.TranscribeResult)
+
+// ReadTranscribeAudio is a convenience function to send audio
+// from the given reader to the stream. Results are processed
+// by the given handler as they come. This function will block
+// until the stream ends, either because the server closed it,
+// or the reader returned io.EOF, or there was an error.
+func ReadTranscribeAudio(
+	stream *TranscribeStream,
+	reader io.Reader,
+	buffSize int,
+	handler TranscribeResultHandler,
+) error {
+	// Read audio on a separate go routine
+	sendErrCh := make(chan error)
+	go func() {
+		sendErrCh <- sendTranscribeAudio(stream, reader, buffSize)
+	}()
+
+	// In the meantime, handle results here
+	var receiveErr error
+	for {
+		var result *diathekepb.TranscribeResult
+		result, receiveErr = stream.ReceiveResult()
+		if receiveErr != nil {
+			break
+		}
+
+		handler(result)
+	}
+
+	// Get the error from the send side of the stream
+	sendErr := <-sendErrCh
+
+	if receiveErr == io.EOF {
+		// This just indicates the stream closed. Return a
+		// send error (if there was one).
+		return sendErr
+	}
+
+	// Both of these are real errors, not io.EOF.
+	if sendErr != nil && receiveErr != nil {
+		return fmt.Errorf("send error: %v, receive error: %v",
+			sendErr, receiveErr)
+	}
+
+	if sendErr != nil {
+		return sendErr
+	}
+
+	return receiveErr
+}
+
+// helper function to handle the send side of the transcribe stream.
+func sendTranscribeAudio(
+	stream *TranscribeStream,
+	reader io.Reader,
+	buffSize int,
+) error {
+	buffer := make([]byte, buffSize)
+	var err error
+	var bytesRead int
+	for {
+		// Pull audio data from the reader
+		bytesRead, err = reader.Read(buffer)
+		if err != nil {
+			break
+		}
+
+		// Send the audio to the stream.
+		err = stream.SendAudio(buffer[0:bytesRead])
+		if err != nil {
+			break
+		}
+	}
+
+	// Close the stream
+	closeErr := stream.PBStream.CloseSend()
+
+	// This indicates that either the reader or the stream
+	// reached its end. In either case, it is not an error,
+	// so we return the stream close error (if there was one).
+	if err == io.EOF {
+		return closeErr
+	}
+
+	// If there was a close error too, return both errors.
+	if closeErr != nil {
+		return fmt.Errorf("%v, %v", err, closeErr)
+	}
+
+	// This indicates something went wrong with the reader or
+	// the stream.
+	return err
 }
