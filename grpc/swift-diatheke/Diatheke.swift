ba1bff9c156bdbd9a32e2cccce3ee4f7866056ae
Eduard Miniakhmetov <eminiakhmetov@tatinfotech.com>
2021-12-03 18:14:48 +0300

    Update version to v2.1.1

--- grpc/swift-diatheke/Diatheke.swift
+++ grpc/swift-diatheke/Diatheke.swift
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-public let DiathekeSDKVersion = "2.1.0"
+public let DiathekeSDKVersion = "2.1.1"
